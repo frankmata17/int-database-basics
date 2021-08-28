ba3aa123e098852a325b26f8a7973c840628a858
Francisco Mata <frank.mata17@gmail.com>
2021-08-28 16:14:02 -0600

    third

--- index.js
+++ index.js
@@ -62,3 +62,40 @@
 // --   SELECT album_id FROM album WHERE artist_id IN (
 // --     SELECT artist_id FROM artist WHERE name = 'Queen'));
 
+// -- UPDATE customer
+// -- SET fax = null
+// -- WHERE fax IS NOT null;
+
+// -- UPDATE customer
+// -- SET company = 'Self'
+// -- WHERE company IS null;
+
+// -- UPDATE customer
+// -- SET last_name = 'Thompson'
+// -- WHERE first_name = 'Julia' AND last_name = 'Barnett';
+
+// -- UPDATE customer
+// -- SET support_rep_id = 4
+// -- WHERE email = 'luisrojas@yahoo.cl';
+
+// -- UPDATE track
+// -- SET composer = 'The darkness around us'
+// -- WHERE genre_id = ( SELECT genre_id FROM genre WHERE name ='Metal')
+// -- AND composer IS null;
+
+// -- SELECT COUNT(*), g.name
+// -- FROM track t
+// -- JOIN genre g ON t.genre_id = g.genre_id
+// -- GROUP BY g.name;
+
+// -- SELECT COUNT(*), g.name
+// -- FROM track t
+// -- JOIN genre g ON g.genre_id = t.genre_id
+// -- WHERE g.name = 'Pop' OR g.name = 'Rock'
+// -- GROUP BY g.name;
+
+// -- SELECT ar.name, COUNT(*)
+// -- FROM album al
+// -- JOIN artist ar ON ar.artist_id = al.artist_id
+// -- GROUP BY ar.name;
+
